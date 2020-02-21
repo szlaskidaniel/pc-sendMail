26c84d6b9175de42b9f190ab4ca449bea17a2ec6
Daniel Szlaski <szlaski.daniel@gmail.com>
2020-02-21 22:35:28 +0100

    update to support autosend

--- README.md
+++ README.md
@@ -8,11 +8,12 @@ Follow below steps to enable this functionality in your org
 * Set Authorized URIs to `https://szlaskidaniel.github.io/pc-sendMail/index.html`
 * Copy created clientId.
 * Create / reuse Inbound Script for Calls
-* Add new WebComponent and set it's url to the `https://localhost/index.html?conversationId={{Scripter.Interaction ID}}&queueId={{Scripter.Queue ID}}&phoneNumber={{Scripter.Customer Formatted Number}}
+* Add new WebComponent and set it's url to the `https://szlaskidaniel.github.io/pc-sendMail/index.html?toAddress={{address}}&toName={{toName}}&queueId={{Scripter.QueueID}}&subject={{Subject}}&htmlBody={{bodyMail}}&wrapUpCodeId={{wrapupCode}}
 
+If you want to use Auto-Send + Auto-Wrapup add &send=true at the end of URL
 
 ### LocalTest
 
-https://localhost/index.html?toAddress=szlaski.daniel@gmail.com&toName=Danielox&queueId=f029fdee-875a-4c21-92bb-f36f1d32b211&subject=TestSubject&htmlBody=ThisIsBody
+https://localhost/index.html?toAddress=daniel.szlaski@genesys.com&toName=Daniel&queueId=f029fdee-875a-4c21-92bb-f36f1d32b211&subject=Thanks%20for%20Buying&htmlBody=TestBody%20With%20spaces%20;)&wrapUpCodeId=0d96421c569748e7b8abdd6433ae6227&send=true
 
 
--- static/js/purecloud.js
+++ static/js/purecloud.js
@@ -1,10 +1,129 @@
 // **** Token Implicit Grant (Browser) - UserLogin ****
-//let redirectUri = 'https://github.com/szlaskidaniel/pc-sendMail/index.html';
-redirectUri = 'https://szlaskidaniel.github.io/pc-sendMail/index.html';
+
+//redirectUri = 'https://szlaskidaniel.github.io/pc-sendMail/index.html';
+const redirectUri = window.location.href;
 const platformClient = require('platformClient');
 const client = platformClient.ApiClient.instance
 client.setPersistSettings(true);
 
+let toAddress = unescape(getUrlVars()["toAddress"]);
+let toName = unescape(getUrlVars()["toName"]);
+let queueId = getUrlVars()["queueId"];
+let subject = unescape(getUrlVars()["subject"]);
+let htmlBody = unescape(getUrlVars()["htmlBody"]);
+let wrapUpCodeId = getUrlVars()["wrapUpCodeId"];
+let send = getUrlVars()["send"];
+
+// Set Environment (in case page reloaded)
+client.setEnvironment("mypurecloud.ie");
+
+let apiInstance = new platformClient.ConversationsApi();
+
+
+// Authenticate & perform Action if URL Param is set to TRUE
+client.loginImplicitGrant("89f29367-dc0e-4fd0-9f55-7175a73ee600", redirectUri, { state: "TEST" })
+    .then(() => {
+        console.log('Logged-In');
+        createEmail();
+
+    })
+    .catch((err) => {
+        // Handle failure response
+        console.log(err);
+    });
+
+function createEmail() {
+    console.log('postConversationDisconnect')
+    return new Promise(function (resolve, reject) {
+        try {
+            let draft_body = {
+                "direction": "OUTBOUND",
+                "queueId": queueId,
+                "toAddress": toAddress,
+                "toName": toName,
+                "subject": subject,
+                "htmlBody": htmlBody
+            };
+
+            apiInstance.postConversationsEmails(draft_body)
+                .then((data) => {
+                    console.log(`postConversationsEmails success! data: ${JSON.stringify(data, null, 2)}`);
+
+                    if (!send) return
+                    let conversationId = data.id; // String | conversationId
+                    var participantId;
+
+
+                    // Get conversationId to retrieve participantAttribute
+                    apiInstance.getConversationsEmail(conversationId)
+                        .then((data) => {
+                            console.log(`getConversationsEmail success! data: ${JSON.stringify(data, null, 2)}`);
+                            data.participants.forEach(function (_participant) {
+                                if (_participant.purpose == 'agent')
+                                    participantId = _participant.id;
+                            })
+
+                            let body = {
+
+                                to: [{
+                                    email: toAddress,
+                                    name: toName,
+                                }],
+                                from: {
+                                    email: "daniel@ininpoland.mypurecloud.ie",
+                                    name: "Daniel"
+                                },
+                                subject: subject,
+                                textBody: htmlBody,
+                                historyIncluded: false
+                            }
+
+                            apiInstance.postConversationsEmailMessages(conversationId, body)
+                                .then((data) => {
+                                    console.log(`postConversationsEmailMessages success! data: ${JSON.stringify(data, null, 2)}`);
+
+                                    let wrapupBody = {
+                                        "wrapup": {
+                                            "code": wrapUpCodeId
+                                        },
+                                        "state": "disconnected"
+                                    }
+                                    // Set Wrapup Code
+                                    apiInstance.patchConversationsEmailParticipant(conversationId, participantId, wrapupBody)
+                                        .then(() => {
+                                            console.log('patchConversationsEmailParticipant returned successfully.');
+                                        })
+                                        .catch((err) => {
+                                            console.log('There was a failure calling patchConversationsEmailParticipant');
+                                            console.error(err);
+                                        });
+
+                                })
+                                .catch((err) => {
+                                    console.log('There was a failure calling postConversationsEmailMessages');
+                                    console.error(err);
+                                });
+
+                        })
+                        .catch((err) => {
+                            console.log('There was a failure calling getConversationsEmail');
+                            console.error(err);
+                        });
+
+                })
+                .catch((err) => {
+                    console.log('There was a failure calling postConversationsEmails');
+                    console.error(err);
+                });
+        } catch (err) {
+            console.log(err);
+            reject(err);
+        }
+    });
+
+}
+
+// help function
 function getUrlVars() {
     var vars = [], hash;
     var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
@@ -14,64 +133,4 @@ function getUrlVars() {
         vars[hash[0]] = hash[1];
     }
     return vars;
-}
-
-let toAddress = getUrlVars()["toAddress"];
-let toName = getUrlVars()["toName"];
-let queueId = getUrlVars()["queueId"];
-let subject = getUrlVars()["subject"];
-let htmlBody = getUrlVars()["htmlBody"];
-
-
-// Set Environment (in case page reloaded)
-client.setEnvironment("mypurecloud.ie");
-
-let apiInstance = new platformClient.ConversationsApi();
-
-
-
-// Authenticate
-client.loginImplicitGrant("8893204d-5404-42d6-8512-8cd91c6ede6a", "https://szlaskidaniel.github.io/pc-sendMail/index.html")
-    .then(() => {
-        // Make request to GET /api/v2/users/me?expand=presence
-        console.log('Logged-In');
-
-    })
-    .catch((err) => {
-        // Handle failure response
-        console.log(err);
-    });
-
-
-//#endregion
-
-
-function createEmail() {
-    console.log('postConversationDisconnect')
-    return new Promise(function (resolve, reject) {
-        try {
-            let body = {
-                "direction": "OUTBOUND",
-                "queueId": queueId,
-                "toAddress": toAddress,
-                "toName": toName,
-                "subject": subject,
-                "htmlBody": htmlBody
-            }; 
-
-            apiInstance.postConversationsEmails(body)
-                .then((data) => {
-                    console.log(`postConversationsEmails success! data: ${JSON.stringify(data, null, 2)}`);
-                })
-                .catch((err) => {
-                    console.log('There was a failure calling postConversationsEmails');
-                    console.error(err);
-                    alert(JSON.stringify(err));
-                });
-        } catch (err) {
-            console.log(err);
-            reject(err);
-        }
-    });
-
 }
